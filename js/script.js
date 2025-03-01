326dc9bea6c6a86082056ddba8014ded955865f7
ShuxratXomidov <xomidovshuxrat@gmail.com>
2025-03-01 20:57:40 +0500

    added user-edit.html

--- js/script.js
+++ js/script.js
@@ -1,35 +1,80 @@
 const domain = "https://localhost:7236/";
 
-
-const div = document.getElementById("navbar");
-
-const navItems = [
-    {name: "Main Page", link: "index.html", class: "nav-link"},
-    {name: "Users Page", link: "user.html", class: "nav-link"},
-    {name: "Products Page", link: "product.html", class: "nav-link"},
-    {name: "Orders Page", link: "order.html", class: "nav-link"},
-    {name: "Roles Page", link: "role.html", class: "nav-link"}
+const navbarMenyu = [
+    {title: "Users Page", page: "user.html"},
+    {title: "Products Page", page: "product.html"},
+    {title: "Orders Page", page: "order.html"},
+    {title: "Roles Page", page: "role.html"}
 ]
 
-function createNavbar(items){
-    const ul = document.createElement("ul");
-    ul.className = "navbar-nav me-auto mb-2 mb-lg-0"
+function createNavbar()
+{
+    const navbar = document.createElement("nav");
+    navbar.className = "navbar navbar-expand-lg navbar-light bg-light";
 
-    items.forEach(item => {
+    const div = document.createElement("div");
+    div.className = "container-fluid";
+
+    const link = document.createElement("a");
+    link.className = "navbar-brand";
+    link.href = "index.html";
+    link.innerText = "Main Page";
+
+    div.prepend(link);
+
+    const btn = document.createElement("button");
+    btn.className = "navbar-toggler";
+    btn.type = "button";
+    btn.dataset.bsToggle = "collapse";
+    btn.dataset.bsTarget = "#navbarSupportedContent";
+    btn.ariaExpanded = false;
+    btn.ariaLabel = "Toggle navigation";
+    btn.ariaControls = "navbarSupportedContent";
+
+    const span = document.createElement("span");
+    span.className = "navbar-toggler-icon";
+
+    btn.appendChild(span);
+
+    const div2 = document.createElement("div");
+    div2.className = "collapse navbar-collapse";
+    div2.id = "navbarSupportedContent";
+
+    const ul = document.createElement("ul");
+    ul.className = "navbar-nav me-auto mb-2 mb-lg-0";
+
+    for (let index = 0; index < navbarMenyu.length; index++)
+    {
         const li = document.createElement("li");
         li.className = "nav-item";
-        const a = document.createElement("a");
-        a.href = item.link;
-        a.textContent = item.name;
-        a.className = item.class;
+        const a = document.createElement("a");  
+        a.ariaCurrent = "page";
+        a.href = navbarMenyu[index].page;
+        a.innerText = navbarMenyu[index].title;
+
+        if(window.location.pathname == "/"+ navbarMenyu[index].page)
+        {
+            a.classList.add("nav-link", "active");
+        }else
+        {
+           a.className = "nav-link";
+        }
 
         li.appendChild(a);
-
         ul.appendChild(li);
-    });
+    }
 
-    div.appendChild(ul);
+    div2.appendChild(ul);
+
+    div.appendChild(btn);
+    div.appendChild(div2);
+
+    navbar.appendChild(div);
+
+    let body = document.querySelector("body");
+    body.prepend(navbar);
 }
+
 
 async function getUsers()
 {
@@ -66,6 +111,51 @@ async function addUser(user)
     }
 }
 
+async function updateUser(id, data)
+{
+    const response = await fetch(domain + "api/User/"+id, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(Object.fromEntries(data))
+    });                                                              // fetch metodga 2 ta parametr berilayapti, birinchisi URL tipi string, ikkinchisi metod berilayapti tipi object.
+    if (response.status == 200){
+        return response.json();
+    }                                                                  // fetch bilan then bu servirga zapros berib servirdan kelgan ma'lumotni oladi
+
+    if (response.status == 404) {
+        return null;                                  // serverga address xato berilganda null qaytadi
+    }
+}
+
+async function deleteUser(id)
+{
+    const response = await fetch(domain + "api/User/"+id, {
+        method: "DELETE"
+    });                                                              // fetch metodga 2 ta parametr berilayapti, birinchisi URL tipi string, ikkinchisi metod berilayapti tipi object.
+    if (response.status == 200){
+        return response.json();
+    }                                                                  // fetch bilan then bu servirga zapros berib servirdan kelgan ma'lumotni oladi
+
+    if (response.status == 404) {
+        return null;                                  // serverga address xato berilganda null qaytadi
+    }
+}
+
+async function getUserId(id)
+{
+
+    const response = await fetch(domain + "api/User/" + id)   // URL orqali servirdan ma'lumot olinayapti
+   if (response.status == 200){
+        return response.json();
+   }                                                                  // fetch bilan then bu servirga zapros berib servirdan kelgan ma'lumotni oladi
+
+   if (response.status == 404) {
+        alert("Page not found!");
+        return null;                                  // serverga address xato berilganda null qaytadi
+   }
+}
 
 async function getProducts() 
 {
@@ -143,67 +233,3 @@ async function getOrders()
         return null;                                  // serverga address xato berilganda null qaytadi
    }
 }
-
-// async function addOrder(order) 
-// {
-//     const response = await fetch("https://localhost:7236/api/Order", {
-//         method: "Post",
-//         body: {
-//             'userId': data.get("userId"),
-//             'productIds': [data.get("productIds")]
-//         },
-//         headers: {
-//             'Content-Type': 'application/json'  
-//         }
-//     })
-
-//     if (response.status == 200)
-//     {
-//         return response.json();
-//     }
-    
-//     if (response.status == 404)
-//     {
-//         return null;
-//     }
-// }
-
-
-
-
-// async function getProducts2() 
-// {
-//     const response = await fetch(domain + "api/Product") 
-//     if (response.status == 200) 
-//     {
-//         return response.json();
-//     } 
-    
-//     if (response.status ==404)
-//     {
-//         return null;
-//     }
-// }
-
-
-// async function addOrder(data) 
-// {
-//     const response = await fetch(domain + "api/Order", {
-//         method: "Post",
-//         headers: {
-//             "Content-Type": "application/json",
-//             "Accept": "accept: */*"
-//         },
-//         body: JSON.stringify(Object.fromEntries(data))
-//     });
-
-//     if (response.ok)
-//     {
-//         return response.json();
-//     }
-//     else
-//     {
-//         alert(response.status);
-//         return null;
-//     }
-// }
